10e48035f0fcc8768d24d70caf746673dc1cf407
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-09 13:10:19 -0700

    ng

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,14 +13,13 @@
  * Description: Prints a random number
  * Return: Always 0 (Success)
  */
-
 int main(void)
 
 {
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;	
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
