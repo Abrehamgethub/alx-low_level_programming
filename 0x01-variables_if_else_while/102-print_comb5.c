b78b5c8000d9f0bd6a0ee76415984d0638cde778
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-09 17:02:41 -0700

    kal

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: program outputs two two digitts possible
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, c;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (c = 0; c < 100; c++)
+		{
+			if (c > i)
+			{
+				putchar((i / 10) + '0');
+				putchar((i % 10) + '0');
+				putchar(' ');
+				putchar((c / 10) + '0');
+				putchar((c % 10) + '0');
+				if (i != 98)
+				{
+					putchar(',');
+					putchar('.');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
