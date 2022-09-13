3e090fc2d8dafda955d83ad067bf61b069adaaa2
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-13 08:18:35 -0700

    smal alpha

---
+++ 1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = ‘a’; ch <= ‘z’; ch++)
+		putchar(ch);
+	putchar (‘\n’);
+	print_alphabet();
+	return (0);
+}
