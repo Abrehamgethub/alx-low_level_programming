830c0c8f04f5a033fb9744aa2ccfa744d4894f7b
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-15 02:26:41 -0700

    fibonaci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,18 +1,24 @@
 #include "main.h"
 /**
- * main - prints count of multiples
- * of 3 or 5 below 1024
+ * main - prints out first
+ * fibonacci suit numbers
  * Return: Always 0
  */
 int main(void)
 {
-	int n, sum = 0;
+	int inc;
+	unsigned long n1 = 0, n2 = 1, n3;
 
-	for (n = 0; n < 1024; n++)
+	for (inc = 0; inc < 50; inc++)
 	{
-		if ((n % 3) == 0 || (n % 5) == 0)
-			sum += n;
+		n3 = n1 + n2;
+		printf("%lu", n3);
+		n1 = n2;
+		n2 = n3;
+		if (inc == 49)
+			printf("\n");
+		else
+			printf(" , ");
 	}
-	printf("%d\n", sum);
 	return (0);
 }
