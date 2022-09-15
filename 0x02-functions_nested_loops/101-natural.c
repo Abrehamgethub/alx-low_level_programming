b8fea718f4db8e88b1e16d7a4c5290916a3d0573
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-15 02:38:58 -0700

    natu

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,7 @@
 /**
  * main - prints count of multiples
  * of 3 or 5 below 1024
- * *Return: return 0
+ * Return: return 0
  */
 int main(void)
 {
@@ -10,7 +10,7 @@ int main(void)
 
 	for (n = 0; n < 1024; n++)
 	{
-		if ((n % 3) == || (n % 5) == 0)
+		if ((n % 3) == 0 || (n % 5) == 0)
 			sum += n;
 	}
 	printf("%d\n", sum);
