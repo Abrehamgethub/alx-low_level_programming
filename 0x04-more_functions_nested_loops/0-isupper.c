bd0407009ed1c65377ab6d73d5301635a0d68253
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-20 01:46:03 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - a function that prints uppercase
+ * @c: harater
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
