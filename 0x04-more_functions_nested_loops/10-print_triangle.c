f3e35fd4dea05a2e1663d18cc9256410166da63b
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-20 04:39:56 -0700

    tria

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - checks main.h file
+ * @size: Size of the triangle
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+		_putchar('\n')
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = size - 1; j > i; j--)
+				_putchar(' ');
+			for (k = 0; k < i + 1; k++);
+			_putchar('#');
+		}
+	}
+}
