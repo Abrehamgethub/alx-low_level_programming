fb37ef6ec1fc448ce699dc262b0ae0373f28a53f
Abreham Kassahun <akabrehamkassahun@gmail.com>
2022-09-20 02:47:11 -0700

    add

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,10 +13,11 @@ void print_diagonal(int n)
 	else
 	{
 		for (i = 0; i < n; i++)
-			[
-				for (j = 0; j < i; j++)
-					_putchar(' ');
-		_putchar('\\');
-		_putchar('\n');
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
 	}
 }
